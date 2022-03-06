3992f6cd72668588093dd9cc135b9390c42564d0
Henrique Ramos <henriqueramos0106@gmail.com>
2022-03-06 10:44:28 -0300

    Ajuste na status bar

--- src/Screens/SchedulingComplete/index.tsx
+++ src/Screens/SchedulingComplete/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useWindowDimensions } from 'react-native';
+import { useWindowDimensions, StatusBar } from 'react-native';
 
 import LogoSvg from '../../assets/logo_background_gray.svg';
 import DoneSvg from '../../assets/done.svg';
@@ -18,6 +18,12 @@ export const SchedulingComplete = () => {
 
   return (
     <Container>
+      <StatusBar 
+        barStyle="light-content"
+        translucent
+        backgroundColor="transparent"
+      />
+       
       <LogoSvg width={width} />
 
       <Content>
